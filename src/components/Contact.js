f9d7666d60e4120b67462dd094dd7de1700d374b
peddy100 <pedram.jarahzadeh@gmail.com>
2024-05-07 21:41:41 -0700

    updated contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -208,7 +208,7 @@ export const Contact = () => {
                 <Col sm={6} className="px-1">
                   <input
                     type="tel"
-                    value={formDetails.lastName}
+                    value={formDetails.phone}
                     placeholder="Phone Number"
                     onChange={(e) => onFormUpdate("phone", e.target.value)}
                   ></input>
